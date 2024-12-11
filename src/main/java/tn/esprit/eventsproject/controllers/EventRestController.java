ae6a411f7753dfcc57a68acf7d8a41c2de7f313b
radhouene101 <radhouene.berrzig@esprit.tn>
2024-12-11 16:13:54 +0100

    webhook 1

--- src/main/java/tn/esprit/eventsproject/controllers/EventRestController.java
+++ src/main/java/tn/esprit/eventsproject/controllers/EventRestController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("event")
 @RestController
-public class EventRestController{
+public class EventRestController {
     @Autowired
     private final IEventServices eventServices;
 
